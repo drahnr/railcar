c1c4ea620148219f6ac40d210e876bd9f97b62dd
Bernhard Schuster <bernhard@ahoi.io>
2019-09-07 15:15:45 +0200

    fix/build: pie is not compatible with static linkage

--- build_seccomp.sh
+++ build_seccomp.sh
@@ -10,6 +10,7 @@ function die {
 [ -e libseccomp/autogen.sh ] || die "libseccomp empty, did you git submodule update --init"
 cd libseccomp
 [ -e ./configure ] || ./autogen.sh || die "autogen failed, check dependencies"
-[ -e ./Makefile ] || CC="musl-gcc -pie -fPIC" CPPFLAGS="-idirafter/usr/include/x86_64-linux-gnu -idirafter/usr/include" ./configure --enable-static
+[ -e ./Makefile ] || CC="musl-gcc" CFLAGS="-fPIC" CPPFLAGS="${CFLAGS} -idirafter/usr/include/x86_64-linux-gnu -idirafter/usr/include" ./configure --enable-static --disable-shared
+
 make || die "make failed, check dependencies"
 echo "$PWD/src/.libs"
